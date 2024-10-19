e9d4bc3ce44243705376c44c6e4b96e3ca6d0c47
Mohamed Naser Mohamed Aldeeb <53155074+mhmaldyb510@users.noreply.github.com>
2024-10-19 18:23:05 +0300

    create custom text field

---
+++ lib/widgets/custom_text_form_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final String labelText;
+  final Icon icon;
+  final TextInputAction? textInputAction;
+  final bool isPassword;
+  const CustomTextFormField({
+    super.key,
+    required this.labelText,
+    required this.icon,
+    this.textInputAction,
+    this.isPassword = false,
+  });
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextFormField(
+        obscureText: widget.isPassword && obscureText,
+        textInputAction: widget.textInputAction,
+        decoration: InputDecoration(
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  icon: Icon(
+                    obscureText ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  })
+              : null,
+          border: const OutlineInputBorder(),
+          labelText: widget.labelText,
+          prefixIcon: widget.icon,
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
