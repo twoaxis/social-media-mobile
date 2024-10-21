df868c312234e4c45bea7d5f9299d2b358f9f0db
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-10-21 10:14:56 +0300

    change the color

--- lib/componant/color.dart
+++ lib/componant/color.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const Color primarycolor =  Color.fromARGB(255, 151, 150, 149);
+const Color primarycolor =  Color(0xFFEEEEEE);
 const Color secondarycolor = Color.fromARGB(255, 195, 48, 48);
