9d581135c7f9fa0994ab71f7b3e0b8792aaa53f9
BiiiiiiiiiiiM <ibrahimabdulqader339@gmail.com>
2024-10-24 00:30:44 +0300

    Add appbar, Bottom navbar & Floating Button (#13)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_mobile/ui/screens/app/menu.dart';
 import 'package:social_media_mobile/ui/screens/onboarding/sign_up_page.dart';
 
 void main() {
   runApp(const SocialMedia());
 }
+
 GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
 class SocialMedia extends StatelessWidget {
@@ -13,7 +15,8 @@ class SocialMedia extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      //  home: SignUpPage(),
+      home: Menu(),
     );
   }
 }
