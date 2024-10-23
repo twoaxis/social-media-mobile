10a979cc42667073405ad816c865828422ae9589
rofidarashed <rofidaelmaghraby@gmail.com>
2024-10-23 20:33:37 +0300

    edited profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_mobile/data/color.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -15,46 +16,108 @@ class _ProfileState extends State<Profile> {
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.arrow_back),
+          color: primarycolor,
         ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 35,
-                backgroundImage: AssetImage('assets/images/icon-user.png'),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Text(
-                  'Username',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const ImageIcon(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                  AssetImage('assets/images/add_friend.png'),
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  follow();
-                },
-                      color: Color.fromARGB(255, 0, 0, 0),
-
-                icon: const Icon(Icons.more_vert),
-              ),
-            ],
-          )
+        centerTitle: true,
+        title: const Text(
+          'Full name',
+          style: TextStyle(
+            color: primarycolor,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: red2,
+        actions: [
+          IconButton(
+            onPressed: () {
+              follow();
+            },
+            color: primarycolor,
+            icon: const Icon(Icons.more_vert),
+          ),
         ],
+      ),
+      backgroundColor: primarycolor,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Stack(
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/background3.png'),
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: CircleAvatar(
+                            radius: 45,
+                            backgroundImage:
+                                AssetImage('assets/images/icon-user.png'),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 7),
+                          child: Text(
+                            '@Username',
+                            style: TextStyle(
+                              color: primarycolor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          'Hello! This is our OS project.',
+                          style: TextStyle(
+                            color: primarycolor,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: secondarycolor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: const Text(
+                                'Add Friend',
+                                style: TextStyle(color: primarycolor),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    backgroundColor: gray),
+                                child: const Text(
+                                  'Follow',
+                                  style: TextStyle(color: primarycolor),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -74,7 +137,7 @@ class _ProfileState extends State<Profile> {
                   child: const Text(
                     'Follow',
                     style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: black,
                       fontSize: 20,
                     ),
                   ),
@@ -84,7 +147,7 @@ class _ProfileState extends State<Profile> {
                   child: const Text(
                     'Followers',
                     style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: black,
                       fontSize: 20,
                     ),
                   ),
@@ -94,7 +157,7 @@ class _ProfileState extends State<Profile> {
                   child: const Text(
                     'Following',
                     style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: black,
                       fontSize: 20,
                     ),
                   ),
