516812c7b3c2440c6fb4f004dd7428806dd87884
Mohamed Naser Mohamed Aldeeb <53155074+mhmaldyb510@users.noreply.github.com>
2024-11-25 22:14:13 +0200

    Home Screen persistence

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:social_media_mobile/ui/screens/onboarding/sign_up_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:social_media_mobile/ui/components/common/scaffold/main_scaffold.dart';
+import 'package:social_media_mobile/ui/screens/onboarding/login_page.dart';
 
 void main() {
   runApp(const SocialMedia());
@@ -12,7 +14,57 @@ class SocialMedia extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+    var navigator = Navigator.of(context);
+    prefs.then(
+      (value) {
+        if (value.getString('authToken') != null &&
+            value.getString('authToken') != '') {
+          navigator.pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const MainScaffold(),
+            ),
+          );
+        } else {
+          navigator.pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const LoginPage(),
+            ),
+          );
+        }
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircularProgressIndicator(),
+            Text('Loading...'),
+          ],
+        ),
+      ),
     );
   }
 }
